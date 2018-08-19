a9221ec1ee91d74a57c2e3ea079a2024b17352fe
Adam Kempler <akempler@gmail.com>
2018-08-18 20:09:21 -0400

    add opening php tag

--- config/drupal/settings.php
+++ config/drupal/settings.php
@@ -1,6 +1,8 @@
+<?php
 
 $config_directories[CONFIG_SYNC_DIRECTORY] = '../config/drupal/sync';
 
 if (file_exists(__DIR__ . '/settings.local.php')) {
   include __DIR__ . '/settings.local.php';
 }
+
